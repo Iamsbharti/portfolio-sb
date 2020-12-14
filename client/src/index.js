97836a139c82fa525eb53093be9edd4709588fce
Iamsbharti <saurabhbharti9@gmail.com>
2020-12-14 13:19:09 +0530

    router and redux init

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import configureStore from "./redux/store";
+import { Provider as ReduxProvider } from "react-redux";
+const store = configureStore();
 ReactDOM.render(
-  <React.StrictMode>
+  <ReduxProvider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </ReduxProvider>,
   document.getElementById("root")
 );
 
