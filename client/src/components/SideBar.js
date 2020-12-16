67021df78a3da8d17f31d68d848055ed08ccbb6e
Iamsbharti <saurabhbharti9@gmail.com>
2020-12-16 12:17:45 +0530

    implemented sidebar

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,5 +1,32 @@
 import React from "react";
-
+import "../css/SideBar.css";
 export const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="sidebar__main">
+      <p>
+        <img src={process.env.PUBLIC_URL + "/shuttle.png"} alt="" />
+      </p>
+      <p>
+        <img src={process.env.PUBLIC_URL + "/icons8-blogger-256.png"} alt="" />
+      </p>
+      <p>
+        <img
+          src={process.env.PUBLIC_URL + "/icons8-certificate-64.png"}
+          alt=""
+        />
+      </p>
+      <p>
+        <img
+          src={process.env.PUBLIC_URL + "/icons8-stack-correctly-96.png"}
+          alt=""
+        />
+      </p>
+      <p>
+        <img
+          src={process.env.PUBLIC_URL + "/icons8-contact-details-96.png"}
+          alt=""
+        />
+      </p>
+    </div>
+  );
 };
