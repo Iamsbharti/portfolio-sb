87441be418a6c3aa9a5d0e6a5e0a9bc11bba13d0
Iamsbharti <saurabhbharti9@gmail.com>
2020-12-21 10:43:40 +0530

    contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export const Contact = () => {
-  return <></>;
+  return (
+    <>
+      <div className="contact__main">
+        <div className="social__icons"></div>
+        <div className="message__box">
+          <input type="text" name="name" placeholder="you name" />
+          <input type="email" name="email" placeholder="you email" />
+          <textarea name="message" placeholder="message" />
+          <div className="message__footer"></div>
+          <button>Send</button>
+          <button>Cancel</button>
+        </div>
+      </div>
+    </>
+  );
 };
